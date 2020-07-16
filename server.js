8bac0b5460736c8963eeb9aea82198b7c35a6c17
chetan goyal <goyalchetan078@gmail.com>
2020-07-16 11:22:51 +0530

    conneting datatbase with knex

--- server.js
+++ server.js
@@ -22,10 +22,8 @@ const profile = require('./controllers/profile');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'dignity',
-    database : 'smart_brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl : true
   }
 });
 
